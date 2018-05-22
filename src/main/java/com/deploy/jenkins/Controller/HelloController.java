c790e379d67a74312ab985bd0920b852e7d53680
Andreas Strid <andreas.strid@lequa.com>
2018-05-22 10:10:28 +0200

    Added local repo to maven

--- src/main/java/com/deploy/jenkins/Controller/HelloController.java
+++ src/main/java/com/deploy/jenkins/Controller/HelloController.java
@@ -3,6 +3,7 @@ package com.deploy.jenkins.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//Comment
 @RequestMapping("/helloController")
 @RestController
 public class HelloController {
